7fc9fc4c3292b091f37ea715b58ae9ee0e813aac
L1ly-42 <l_peltier@outlook.com>
2026-01-12 15:52:10 +0000

    changes

---
+++ 0.Cheatsheets/python_term_dictionary.py
@@ -0,0 +1,27 @@
+#Methods
+"""
+import    : Imports variables,functions etc. onto your program from other modules.
+print     : Prints something to your screen (output value, text, whatever to screen)
+return    : Returns anything from a function to be stored (value, text, whatever)
+str()     : Casts the contents of the function into a string.
+int()     : Casts the contents of the function into an integer.
+float()   : Casts the contents of the function into a float.
+len()     : Counts the number of characters in a string and stores it.
+.upper()  : Converts the contents of the string into all uppercase letters.
+.lower()  : Converts the contents of the string into all lowercase letters.
+max()     : Takes any number arguments (integer or float) and returns the max value.When done on a single string it will return the max alphabetical character in the string - aka last in the alphabet.
+min()     : Takes any number arguments (interger or float) and returns the min value.When done on a single string it will return the min alphabetical character in the string- aka first in the alphabet.
+sorted()  : Returns a copy of a list in order from smallest to largest, leaving the list unchanged.
+abs()     : Returns the absolute value of the number it takes as an argument—that is, that number's distance from 0 on an imagined number line. Only takes a single number.
+type()    : Returns the type of data it recieves as an argument.
+.append() : Adds new items to the end of lists.
+range()   : A built in function that returns a sequence of integers from 0 up until but not including the number put into it - kind of like indexes. 
+                In fact you can also splice numbers with this the same way you do with advanced indexes! e.g range(2,10,2) = 2468
+
+get()     : Looks up values in a dictionary returns None (or a default value of your choice) if the key isn't found, and will retrieve the key's value if it IS in the dictionary.
+              This is good for retrieving values for a key you aren't sure are there!
+              You can just use this method and if its not there you will get "none" instead of an error!
+           
+
+
+"""
